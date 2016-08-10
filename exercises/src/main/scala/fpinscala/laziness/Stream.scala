b380f91cc9b7671ef1173cf1a43bf91d6191bead
Benen Cahill <benen.cahill@gmail.com>
2016-08-10 09:04:18 +0100

    Ex 5.3

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -26,21 +26,43 @@ trait Stream[+A] {
 
   /* Ex 5.2 i */
   def take(n: Int): Stream[A] = this match {
+    case Empty if n > 0 => throw new NoSuchElementException("Stream is empty")
     case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
     case _ => Empty
   }
 
   /* Ex 5.2 ii */
-  def drop(n: Int): Stream[A] = this match {
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 0 => t().drop(n - 1)
     case _ => this
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  /* Ex 5.3 */
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
 
   def forAll(p: A => Boolean): Boolean = sys.error("todo")
 
   def headOption: Option[A] = sys.error("todo")
+
+  def map[B](f: A => B): Stream[B] = sys.error("todo")
+
+  def filter(p: A => Boolean): Stream[A] = sys.error("todo")
+
+  def append[B >: A](that: => Stream[B]): Stream[B] = sys.error("todo")
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = sys.error("todo")
+
+  def zipWith[B,C](s2: Stream[B])(f: (A,B) => C): Stream[C] = sys.error("todo")
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = sys.error("todo")
+
+  def tails: Stream[Stream[A]] = sys.error("todo")
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = sys.error("todo")
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
@@ -67,4 +89,8 @@ object Stream {
   def from(n: Int): Stream[Int] = sys.error("todo")
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+
+  def constant[A](a: A): Stream[A] = sys.error("todo")
+
+  lazy val fibs: Stream[Int] = sys.error("todo")
 }
