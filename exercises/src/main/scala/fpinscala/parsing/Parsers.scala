e3802946202f7ac7b34e9297fd20f915fbd4c278
Benen Cahill <benen.cahill@gmail.com>
2017-10-11 09:19:27 +0100

    Ch. 9: Sketching

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -2,14 +2,42 @@ package fpinscala.parsing
 
 import language.higherKinds
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait Parsers[ParseError, Parser[+_]] { self => // so inner classes may call methods of trait
 
-  case class ParserOps[A](p: Parser[A]) {
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def char(c: Char): Parser[Char]
+  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+  def listOfN[A](i: Int, parser: Parser[A]): Parser[List[A]]
+
+  def int(i:  Int): Parser[Int]
+  def zeroOrMore[A](p: Parser[A]): Parser[Int] = or(oneOrMore(p), int(0))
+  def oneOrMore[A](p: Parser[A]): Parser[Int]
+  def andThen[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+  def exists[A](p: Parser[A]): Parser[Boolean]
+  def zomOrOom[A](p1: Parser[A], p2: Parser[A]): Parser[(Int, Int)] = andThen(zeroOrMore(p1), )
+
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  object Laws {
+
+    val s1 = "abra"
+    val s2 = "cadabra"
+
+    run(string(s1))(s1) == Right(s1)
+    run(or(string(s1), string(s2)))(s1) == Right(s1)
+    run(or(string(s1), string(s2)))(s2) == Right(s2)
+    run(listOfN(3, or(string(s1), string(s2))))(s1 + s2 + s1) == Right(s1 + s2 + s1)
 
 
   }
 
-  object Laws {
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def &[B>:A](p2: Parser[B]): Parser[B] = self.andThen(p, p2)
+    def and[B>:A](p2: => Parser[B]): Parser[B] = self.andThen(p, p2)
   }
 }
 
